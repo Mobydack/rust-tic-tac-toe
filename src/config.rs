2476819638604ed7f30bf5afbc9512b5b4f6f6b7
mobydack <devzetsubou@gmail.com>
2022-04-17 22:01:41 +0300

    feat: add base logic for window

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+use winit::dpi::LogicalSize;
+
+pub const WINDOW_TITLE: &str = "Tic-Tac-Toe";
+pub const DEFAULT_INNER_SIZE: LogicalSize<u16> = LogicalSize::new(300, 300);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod config;
+
+use winit::{
+    event::{Event, WindowEvent},
+    window::WindowBuilder,
+    event_loop::{EventLoop, ControlFlow},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop_instance = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title(config::WINDOW_TITLE)
+        .with_inner_size(config::DEFAULT_INNER_SIZE)
+        .build(&event_loop_instance)
+        .unwrap();
+
+    event_loop_instance.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit
+            }
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            _ => ()
+        }
+    });
 }
